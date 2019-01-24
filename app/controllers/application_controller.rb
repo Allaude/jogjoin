3861cc1688d032b788bea78a3143f4e25bf31bea
allaude <riyan.stmik28@gmail.com>
2019-01-25 01:41:47 +0700

    cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::API
   include Knock::Authenticable
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+       origins '*'
+       resource '*', :headers => :any, :methods => [:get, :post, :options]
+     end
+  end
 end
