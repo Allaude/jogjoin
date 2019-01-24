6a814f22adf5fa4e252699f5423a4920c50dcc80
allaude <riyan.stmik28@gmail.com>
2019-01-25 00:18:21 +0700

    cors v2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::API
   include Knock::Authenticable
-  config.middleware.insert_before 0, Rack::Cors do
-    allow do
-       origins '*'
-       resource '*', :headers => :any, :methods => [:get, :post, :options]
-     end
-  end
+  headers['Access-Control-Allow-Origin'] = '*'
+  headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+  headers['Access-Control-Request-Method'] = '*'
+  headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
 end
